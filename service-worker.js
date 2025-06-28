a83ed88bf8bd19ae62c2c6ce527a516dd0d301f8
licenseplategame <aliasheaven0@gmail.com>
2025-06-27 22:46:00 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,34 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("plate-cache").then(cache => {
-      return cache.addAll(["./", "./index.html", "./styles.css", "./app.js", "./data.js"]);
-    })
+const CACHE_NAME = 'plate-cache-v2';  // note "v2" to force update
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './data.js',
+  './manifest.json',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+        .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
